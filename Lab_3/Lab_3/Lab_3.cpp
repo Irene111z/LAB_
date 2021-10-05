f8e0cfe422873cf80081b292eb224556d53ef8e8
Irene111z <Iryna.Zmiyevska@cs.khpi.edu.ua>
2021-10-05 20:44:26 +0300

    Лабораторная работа №3

---
+++ Lab_3/Lab_3/Lab_3.cpp
@@ -0,0 +1,31 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main(void) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	// a≠ 0, n≠ 0, x ≠ 0
+	double a = 5.5, b = 2.3;
+	int n = 3;
+	double x;       //parameter which you need to enter
+	double t1, t2;  //our result
+
+	printf("Введите: x (обратите внимание, что x не может принимать значение 0) >");//4
+	scanf("%lf", &x);
+
+	double G2 = pow(x, n - 2) * (n - 2);//temporary var
+	double G1 = pow(x, n - 1) * (n - 1);//temporary var
+
+	t1 = (1 / a) * ((-1 / G2) + (b / G1));//-0,32
+
+	double ax = a * x;//temporary var
+	double k = ((pow(x, 2) / a) - (2 / pow(a, 3)));
+	
+	t2 = ((2 * x) / pow(a, 2)) * sin(ax) - k * cos(ax);//2,89
+
+	printf("t1 = %lg\n", t1);
+	printf("t2 = %lg\n", t2);
+		return 0;
+}
