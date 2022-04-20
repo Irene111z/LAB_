70a89048a003dd18f07a73b0104578792b226054
Irene111z <Iryna.Zmiyevska@cs.khpi.edu.ua>
2022-04-20 14:08:07 +0300

    Строки

---
+++ 2s/Lab_6(2s)/Lab_6(2s)/Lab_6(2s).cpp
@@ -0,0 +1,77 @@
+﻿#include "foo.h"
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	while (true)
+	{
+		
+		char S[N];
+		char S1[N];
+		int L;
+
+		system("cls");
+		system("color 7");
+		cout << "Введите строку: ";
+		gets_s(S, N);
+		int n = strlen(S) + 1;
+		
+			if (strlen(S) < 1)
+			{
+				HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleTextAttribute(handle, FOREGROUND_RED);
+				cout << "Строка должна содержать минимум один символ..." << endl;
+				system("pause");
+				continue;
+			}
+			if (strlen(S) + 1 > N)
+			{
+				HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleTextAttribute(handle, FOREGROUND_RED);
+				cout << "Ваша строка превышает лимит ( " << N << " ) " << "символов..." << endl;
+				cout << "Длина вашей строки: " << strlen(S) + 1 << endl;
+				system("pause");
+				cin.ignore();
+				continue;
+				
+
+			}
+			
+			system("color 7");
+			cout << "Введите длину выравнивания: ";
+			cin >> L;
+
+			if (strlen(S) < L)
+			{
+				HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleTextAttribute(handle, FOREGROUND_RED);
+				cout << "Выберите длину меньше длины строки..." << endl;
+				cout << "Длина вашей строки: " << strlen(S) + 1 << endl;
+				system("pause");
+				cin.ignore();
+				continue;
+				
+			}
+			if (L <= 0)
+			{
+				HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleTextAttribute(handle, FOREGROUND_RED);
+				cout << "Минимальное значение длины выравнивания = 1..." << endl;
+				system("pause");
+				cin.ignore();
+				continue;
+			
+			}
+				
+		left(S, S1, n, L);
+		puts(S1);
+		system("pause");
+		cin.ignore();
+			
+			
+		
+	}
+	
+}
